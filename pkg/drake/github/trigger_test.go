ca7eb5234d2bddc5b755b2cf3581d25f09ce13e7
Kent Rancourt <kent.rancourt@gmail.com>
2019-10-07 16:32:02 -0400

    improved github trigger
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- pkg/drake/github/trigger_test.go
+++ pkg/drake/github/trigger_test.go
@@ -11,124 +11,167 @@ func TestMatches(t *testing.T) {
 	testCases := []struct {
 		name       string
 		trigger    *trigger
-		assertions func(*testing.T, *trigger)
+		event      brigade.Event
+		assertions func(*testing.T, bool, error)
 	}{
 		{
-			name: "a trigger that tests PRs",
-			trigger: &trigger{
-				BranchSelector: &refSelector{
-					BlacklistedRefs: []string{"master"},
-				},
+			name:    "non-github event",
+			trigger: &trigger{},
+			event: brigade.Event{
+				Provider: "bitbucket",
 			},
-			assertions: func(t *testing.T, trigger *trigger) {
-				// Looks like a PR
-				matches, err := trigger.Matches(
-					brigade.Event{
-						Provider: "github",
-						Type:     "check_suite:rerequested",
-						Payload:  []byte(`{"body":{"action":"rerequested","check_suite":{"head_branch":null}}}`), // nolint: lll
-					},
-				)
-				require.NoError(t, err)
-				require.True(t, matches)
-				// Looks like a merge to master
-				matches, err = trigger.Matches(
-					brigade.Event{
-						Provider: "github",
-						Type:     "check_suite:requested",
-						Payload:  []byte(`{"body":{"action":"requested","check_suite":{"head_branch":"master"}}}`), // nolint: lll
-					},
-				)
-				require.NoError(t, err)
-				require.False(t, matches)
-				// Looks like a release
-				matches, err = trigger.Matches(
-					brigade.Event{
-						Provider: "github",
-						Type:     "push",
-						Payload:  []byte(`{"ref":"refs/tags/v0.0.1"}`),
-					},
-				)
+			assertions: func(t *testing.T, matches bool, err error) {
 				require.NoError(t, err)
 				require.False(t, matches)
 			},
 		},
 		{
-			name: "a pipeline that tests the master branch",
-			trigger: &trigger{
-				BranchSelector: &refSelector{
-					WhitelistedRefs: []string{"master"},
-				},
+			name:    "unsupported event type",
+			trigger: &trigger{},
+			event: brigade.Event{
+				Provider: "github",
+				Type:     "pull_request",
 			},
-			assertions: func(t *testing.T, trigger *trigger) {
-				// Looks like a PR
-				matches, err := trigger.Matches(
-					brigade.Event{
-						Provider: "github",
-						Type:     "check_suite:rerequested",
-						Payload:  []byte(`{"body":{"action":"rerequested","check_suite":{"head_branch":null}}}`), // nolint: lll
-					},
-				)
-				require.NoError(t, err)
-				require.False(t, matches)
-				// Looks like a merge to master
-				matches, err = trigger.Matches(
-					brigade.Event{
-						Provider: "github",
-						Type:     "check_suite:requested",
-						Payload:  []byte(`{"body":{"action":"requested","check_suite":{"head_branch":"master"}}}`), // nolint: lll
-					},
-				)
-				require.NoError(t, err)
-				require.True(t, matches)
-				// Looks like a release
-				matches, err = trigger.Matches(
-					brigade.Event{
-						Provider: "github",
-						Type:     "push",
-						Payload:  []byte(`{"ref":"refs/tags/v0.0.1"}`),
-					},
-				)
+			assertions: func(t *testing.T, matches bool, err error) {
 				require.NoError(t, err)
 				require.False(t, matches)
 			},
 		},
 		{
-			name: "a pipeline for executing a release",
+			name: "check suite request event with unconfigured check suite " +
+				"request event selector",
+			trigger: &trigger{},
+			event: brigade.Event{
+				Provider: "github",
+				Type:     "check_suite:requested",
+			},
+			assertions: func(t *testing.T, matches bool, err error) {
+				require.NoError(t, err)
+				require.False(t, matches)
+			},
+		},
+		{
+			name: "check suite request event with unconfigured branch selector",
 			trigger: &trigger{
-				TagSelector: &refSelector{
-					WhitelistedRefs: []string{`/v[0-9]+(\.[0-9]+)*(\-.+)?/`},
+				CheckSuiteRequestEventSelector: &checkSuiteRequestEventSelector{},
+			},
+			event: brigade.Event{
+				Provider: "github",
+				Type:     "check_suite:requested",
+			},
+			assertions: func(t *testing.T, matches bool, err error) {
+				require.NoError(t, err)
+				require.False(t, matches)
+			},
+		},
+		{
+			name: "check suite request event that does not match trigger",
+			trigger: &trigger{
+				CheckSuiteRequestEventSelector: &checkSuiteRequestEventSelector{
+					BranchSelector: &refSelector{
+						WhitelistedRefs: []string{"master"},
+					},
 				},
 			},
-			assertions: func(t *testing.T, trigger *trigger) {
-				// Looks like a PR
-				matches, err := trigger.Matches(
-					brigade.Event{
-						Provider: "github",
-						Type:     "check_suite:rerequested",
-						Payload:  []byte(`{"body":{"action":"rerequested","check_suite":{"head_branch":null}}}`), // nolint: lll
-					},
-				)
+			event: brigade.Event{
+				Provider: "github",
+				Type:     "check_suite:requested",
+				// Looks like a check suite request for a commit from a fork
+				Payload: []byte(`{"body":{"action":"rerequested","check_suite":{"head_branch":null}}}`), // nolint: lll
+			},
+			assertions: func(t *testing.T, matches bool, err error) {
 				require.NoError(t, err)
 				require.False(t, matches)
-				// Looks like a merge to master
-				matches, err = trigger.Matches(
-					brigade.Event{
-						Provider: "github",
-						Type:     "check_suite:requested",
-						Payload:  []byte(`{"body":{"action":"requested","check_suite":{"head_branch":"master"}}}`), // nolint: lll
+			},
+		},
+		{
+			name: "check suite request event that matches trigger",
+			trigger: &trigger{
+				CheckSuiteRequestEventSelector: &checkSuiteRequestEventSelector{
+					BranchSelector: &refSelector{
+						WhitelistedRefs: []string{"master"},
 					},
-				)
+				},
+			},
+			event: brigade.Event{
+				Provider: "github",
+				Type:     "check_suite:requested",
+				// Looks like a check suite request for a commit on master
+				Payload: []byte(`{"body":{"action":"rerequested","check_suite":{"head_branch":"master"}}}`), // nolint: lll
+			},
+			assertions: func(t *testing.T, matches bool, err error) {
+				require.NoError(t, err)
+				require.True(t, matches)
+			},
+		},
+		{
+			name: "push event with unconfigured tag push event selector",
+			trigger: &trigger{
+				TagPushEventSelector: &tagPushEventSelector{},
+			},
+			event: brigade.Event{
+				Provider: "github",
+				Type:     "push",
+			},
+			assertions: func(t *testing.T, matches bool, err error) {
 				require.NoError(t, err)
 				require.False(t, matches)
-				// Looks like a release
-				matches, err = trigger.Matches(
-					brigade.Event{
-						Provider: "github",
-						Type:     "push",
-						Payload:  []byte(`{"ref":"refs/tags/v0.0.1"}`),
+			},
+		},
+		{
+			name: "push event that isn't for a new tag",
+			trigger: &trigger{
+				TagPushEventSelector: &tagPushEventSelector{
+					TagSelector: &refSelector{
+						WhitelistedRefs: []string{"v0.0.1"},
 					},
-				)
+				},
+			},
+			event: brigade.Event{
+				Provider: "github",
+				Type:     "push",
+				// Looks like a push request that isn't for a new tag
+				Payload: []byte(`{"ref":"refs/head/master"}`),
+			},
+			assertions: func(t *testing.T, matches bool, err error) {
+				require.NoError(t, err)
+				require.False(t, matches)
+			},
+		},
+		{
+			name: "push event that does not match trigger",
+			trigger: &trigger{
+				TagPushEventSelector: &tagPushEventSelector{
+					TagSelector: &refSelector{
+						WhitelistedRefs: []string{"v0.0.1"},
+					},
+				},
+			},
+			event: brigade.Event{
+				Provider: "github",
+				Type:     "push",
+				Payload:  []byte(`{"ref":"refs/tags/foobar"}`),
+			},
+			assertions: func(t *testing.T, matches bool, err error) {
+				require.NoError(t, err)
+				require.False(t, matches)
+			},
+		},
+		{
+			name: "push event that matches trigger",
+			trigger: &trigger{
+				TagPushEventSelector: &tagPushEventSelector{
+					TagSelector: &refSelector{
+						WhitelistedRefs: []string{"v0.0.1"},
+					},
+				},
+			},
+			event: brigade.Event{
+				Provider: "github",
+				Type:     "push",
+				Payload:  []byte(`{"ref":"refs/tags/v0.0.1"}`),
+			},
+			assertions: func(t *testing.T, matches bool, err error) {
 				require.NoError(t, err)
 				require.True(t, matches)
 			},
@@ -137,7 +180,8 @@ func TestMatches(t *testing.T) {
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			testCase.assertions(t, testCase.trigger)
+			matches, err := testCase.trigger.Matches(testCase.event)
+			testCase.assertions(t, matches, err)
 		})
 	}
 }
