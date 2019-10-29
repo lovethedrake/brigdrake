83c9a7689e28c48326331f250814c69f784b0cc5
Kent Rancourt <kent.rancourt@gmail.com>
2019-10-29 17:30:00 -0400

    fix issue with chart publishing script
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- scripts/publish-chart.sh
+++ scripts/publish-chart.sh
@@ -8,7 +8,9 @@ npm install -g gh-pages@2.0.1
 
 set +x # Don't let the value of $GITHUB_TOKEN bleed into the logs!
 
-gh-pages --add -d /shared/chart/dist \
+cp -r /shared/chart/dist chart/dist
+
+gh-pages --add -d chart/dist \
   -r https://drakeci:$GITHUB_TOKEN@github.com/lovethedrake/brigdrake.git \
   -u "Drake CI <drake@ci>" \
   -m "Add chart artifacts and update index.yaml"
